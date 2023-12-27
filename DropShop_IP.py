3c1c616d2f6fdc33e13d52b30bbaede61f76a70e
BrannonKLuong <146150571+BrannonKLuong@users.noreply.github.com>
2023-12-26 18:39:22 -0800

    Add files via upload
    12/26/2023 6:37pm the current issue is that when reviewing the code the frame timing for which
    The droplets are initialized are in a different order. The algorithm assumes droplets are sorted in distance travels. so right now at frame 
    157 droplets are in order of [1, 2, 3, 4] when it should be [1, 2, 4, 3] where the number is their order inserted and the ordering the distance in 
    the course
    
    Most of the binary search algorithm is in place

--- DropShop_IP.py
+++ DropShop_IP.py
@@ -5,20 +5,37 @@ import supervision as sv
 import sys, os
 import math
 import time
+import math
 
 class Path():
     def __init__(self) -> None:
         self.segments_in_order = []
         "This dictionary maps every segment to an idx"
-        self.segments_index_map = {}
+        self.segments_index_map = {} #Maps a segment to it's (index, distance traveled)
         self.idx = 0
     
     def add_segment(self, new_segment) -> None:
-        self.segments_index_map[new_segment] = self.idx
+        self.segments_index_map[new_segment] = (self.idx, self.distance_traversed())
         self.idx += 1
         "This assigns the index"
         self.segments_in_order.append(new_segment)
-
+    
+    def distance_traversed(self):
+        if not self.idx:
+            return 0
+        else:
+            distance_traveled = 0
+            for i in range(self.idx):
+                seg = self.segments_in_order[i]
+                if isinstance(seg, Straight):
+                    if seg.direction[0]: #if it's going left or right
+                        distance_traveled += seg.bottom_right[0] - seg.top_left[0]
+                    else: #If it's going up or down
+                        distance_traveled += seg.bottom_right[1] - seg.top_left[1]
+                else:
+                    distance_traveled += get_distance(seg.start, seg.end)
+            return distance_traveled
+                            
 class Droplet():
     def __init__(self, id, x: int = None, y:int = None, trajectory: int = 1, current_section: int = 0) -> None:
         self.id = id
@@ -55,7 +72,7 @@ class Droplet():
                 # This new section uses the index of the segment it was detected in as opposed to incrementing it
                 # Implemented this because in the video when the dispensers are fired there's a chance the droplets move backwards up the channel
                 try:
-                    self.current_section = course.segments_index_map[map_of_segments[(self.x, self.y)]]
+                    self.current_section = course.segments_index_map[map_of_segments[(self.x, self.y)]][0]
                 except:
                     # Adding this clause in the case the detection is outside of the course if the box is not perfect it will remain what the current section of the droplet is
                     # Might have to assign it to be the segment closest to it.
@@ -138,18 +155,92 @@ class Curve():
         a, b, c = self.quadratic_coef
         return a * (x ** 2) + b * x + c
     
-def find_closest_droplet(drops_to_consider: {Droplet}, mid:(int, int)) -> Droplet:
-    closest = float('inf')
-    closest_drop = None
-    for drop in drops_to_consider:
-        # print()
-        drop_point = (drop.x, drop.y)
-        distance = get_distance(drop_point, mid) 
-        if distance < closest:
-            closest_drop = drop
-            closest = distance
+def find_closest_droplet(drops_to_consider: {Droplet}, mid:(int, int), course, x_y_map) -> Droplet:
+    '''As of right now the algorithm to find the closest droplet is Brute Force O(n^2)
+    Designing a Recursive/Iterative Algorithm'''
+    acceptable_distance = 5 #Some arbitrarily chosen acceptable distance 
+    l, r = 0, len(drops_to_consider) - 1
+    print([drop.id for drop in drops_to_consider]) #12/26/2023 6:37pm the current issue is that when reviewing the code the frame timing for which
+    #The droplets are initialized are in a different order. The algorithm assumes droplets are sorted in distance travels. so right now at frame 
+    # 157 droplets are in order of [1, 2, 3, 4] when it should be [1, 2, 4, 3] where the number is their order inserted and the ordering the distance in 
+    # the course
+    arr = drops_to_consider
+    while l <= r:
+        if len(arr) == 1:
+            return arr[0]
+        if l == r: #If pointers pointing to the same droplet
+            return arr[l]   
+        m = (l + r)//2
+        m_d = arr[m] #m_d is middle droplet the naming is trying to not reassign the variable mid
+        print()
+        print(f"(Left, Right): {l, r}")
+        print(f"Left: {[drop.id for drop in arr[:m]]} Right:{[drop.id for drop in arr[m:]]}.")   
+        calc_dist = get_distance((m_d.x, m_d.y), mid)   
+        # print(f"(Left, Right): {l, r}")
+        # print(f"Calculated Distant from Detection to Current Droplet {m_d.id}: {calc_dist}")  
+        if calc_dist <= acceptable_distance: #If the detection is within a reasonable detection to a droplet assume that's the closest and return it
+            return m_d
+        else:
+            #Otherwise compare it's distance from the left most droplet and right most droplet
+            
+            #This isn't adding the droplet distance itself yet
+            left_distance = determine_total_distance_traveled((arr[l].x, arr[l].y), course.segments_in_order[arr[l].current_section], course)
+            detection_distance = determine_total_distance_traveled(mid, x_y_map[mid], course)
+            right_distance = determine_total_distance_traveled((arr[r].x, arr[r].y),course.segments_in_order[arr[r].current_section], course)
+            right_difference_detection = abs(right_distance - detection_distance)
+            left_difference_detection = abs(detection_distance - left_distance)
+            print(f"Detection Coordinate: {mid}")
+            print(f"Distance Detection Traveled: {detection_distance}")
+            print(f"Distance Left Droplet Traveled: {left_distance}   Difference: {left_difference_detection}")
+            print(f"Distance Right Droplet Traveled: {right_distance}  Difference: {right_difference_detection}")
+            if right_difference_detection <= acceptable_distance:
+                return arr[r]
+            if  left_difference_detection  <= acceptable_distance:
+                return arr[l]
+            if len(arr) == 2:
+                if right_difference_detection < left_difference_detection:
+                    return arr[r]
+                else:
+                    return arr[l]
+            else:
+                if right_difference_detection < left_difference_detection: 
+                    #If the detection is closer to the right most 
+                    #Ignore the left half
+                    l = m + 1
+                else:
+                    r = m - 1
+                print(f"New (Left, Right): {l, r}")
+        # Otherwise compare the distance between left and right
+       
+        
 
-    return closest_drop  
+def determine_total_distance_traveled(coordinate, curr_seg, course): #Coordinate can be droplet coordinate or 
+    #Now let's get distance already traveled
+    if not coordinate:
+        return 0
+    distanced_traveled = course.segments_index_map[curr_seg][1] #if n - 1 segments have already been traversed add the flat sum of n segments to the distanced traveled
+    #Now calculate the distanced traveled in the current nth segment
+    #Remember the grid is inverted 0, 0 is the top left so we can use math.abs to get the absolute value of the distanced traveled since the start will be on the right, traveling left the det x should be < than the right most x
+    if isinstance(curr_seg, Straight): #if it's a straight
+        horiz, vert = curr_seg.direction #Break the tuple into the horizontal, vertical direction
+        if horiz and not vert: #If traveling horizontally and left <------------------------------- IMPORTANT: The Direction Tuple is in format of (1, 0) (0, 1) (-1, 0) (0, -1) denoting right, down, left, up
+            if horiz == -1:
+                #Add the distance from the detection's x to the right most x of the segment
+                right_most_x = curr_seg.bottom_right[0]
+                distanced_traveled += right_most_x - coordinate[0]
+            else: # if traveling right
+                #Add the distance from detection's x to the left msot x of the segment since traveling right det_x > left_most_x
+                left_most_x = curr_seg.top_left[0]
+                distanced_traveled += coordinate[0] - left_most_x 
+        else: # Traveling vertically
+            if vert == -1: #If traveling up: Subtract the detection from the bottom most y. Since Detection y < bottom most y
+                bottom_most_y = curr_seg.bottom_right[1] 
+                distanced_traveled += bottom_most_y - coordinate[1]
+            else: #if traveling down, top most y < det y
+                top_most_y = curr_seg.top_left[1]
+                distanced_traveled += coordinate[1]  - top_most_y
+    return distanced_traveled
+
 
 def load_mac_files():
     model = YOLO("runs/detect/train10/weights/best.pt")
@@ -195,35 +286,35 @@ def label_curves_s_m_e(frame, course) -> None:
 def get_droplets_on_screen(t : int, num_droplets: int, drops:{Droplet}, course) -> int:
     if t == 1:
         droplet_1 = Droplet(1, 450, 60, 2)
-        drops.add(droplet_1)
+        drops.append(droplet_1)
         return 1
-    elif t == 114:
+    elif t == 119:
         droplet_2 = Droplet(2, 315, 60, 1)
-        drops.add(droplet_2)
+        drops.append(droplet_2)
         return 2
-    elif t == 147:
-        droplet_3 = Droplet(3, 315, 60, 1)
-        drops.add(droplet_3)
+    elif t == 153:
+        droplet_3 = Droplet(3, 450, 60, 1)
+        drops.append(droplet_3)
         return 3
-    elif t == 152:
-        droplet_4 = Droplet(4, 450, 60, 1)
-        drops.add(droplet_4)
+    elif t == 156:
+        droplet_4 = Droplet(4, 315, 60, 1)
+        drops.append(droplet_4)
         return 4
     elif t == 185:
         droplet_5 = Droplet(5, 450, 60, .5)
-        drops.add(droplet_5)
+        drops.append(droplet_5)
         return 5
     elif t == 222:
         droplet_6 = Droplet(6, 450, 60, .5)
-        drops.add(droplet_6)
+        drops.append(droplet_6)
         return 6
     elif t == 370:
         droplet_7 = Droplet(7, 460, 195, 1, 4)
-        drops.add(droplet_7)
+        drops.append(droplet_7)
         return 7
     elif t == 515:
         droplet_8 = Droplet(8, 315, 195, 1, 4)
-        drops.add(droplet_8)
+        drops.append(droplet_8)
         return 8 
     else:
         return num_droplets
@@ -274,8 +365,9 @@ def load_mac_files():
     return model, video_cap 
 
 def main(weights_path, video_path):
-    all_droplets = set()
+    all_droplets = []
     course = build_course()
+    # print(course.segments_index_map)
     x_y_map = build_x_y_map(course)
     box = sv.BoxAnnotator(text_scale=0.3)
     speed_threshold = 5
@@ -315,8 +407,7 @@ def main(weights_path, video_path):
                     try:
                         '''drops to consider is ideally always the drops in the segment closest to the detection'''
                         mid = get_mid_point(xone, yone, xtwo, ytwo)
-                        drops_to_consider = all_droplets
-
+                        
                     except KeyError:
                         print("Detection occurred outside of the course. Data: ", data)
                         continue
@@ -326,7 +417,7 @@ def main(weights_path, video_path):
                         print(exc_type, fname, exc_tb.tb_lineno)
                         continue
                     
-                    closest_droplet = find_closest_droplet(drops_to_consider, mid)
+                    closest_droplet = find_closest_droplet(all_droplets, mid, course, x_y_map)
                     # try:
                     #     print("Closest Droplet: " +  str(closest_droplet.id) + " Current Section: " + str(closest_droplet.current_section))
                     # except:
@@ -334,7 +425,7 @@ def main(weights_path, video_path):
                     found.add(closest_droplet)
 
                     closest_droplet.update_last_seen(mid, t, x_y_map, speed_threshold)
-                  
+                
                     if x_y_map[mid] != course.segments_in_order[closest_droplet.current_section]:
                         closest_droplet.update_section(course, x_y_map)
                     
